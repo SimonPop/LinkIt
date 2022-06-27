9d7bf90f97566ab2ba3e93ba441c8d14a09bd7e2
simon <simon.popelier@gmail.com>
2022-06-27 21:24:56 +0200

    Package files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './front/App';
+import reportWebVitals from './front/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
